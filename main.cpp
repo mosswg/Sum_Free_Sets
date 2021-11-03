26166fa1aeef385c001348e9b2f9ba9ea67dde0a
Moss Gallagher <moss@mossx.net>
2021-11-03 10:25:00 -0400

    Combined generate_powersets and is_complete_sum_free_set
    Created get_complete_sum_free_sets functions

--- main.cpp
+++ main.cpp
@@ -45,15 +45,57 @@ template <typename t> std::ostream& operator<<(std::ostream& out, const std::vec
 	return out;
 }
 
+template <typename t> bool vector_contains(std::vector<t> const& vec, t const& search) {
+	return std::any_of(vec.begin(), vec.end(), [search](auto const& element){return element == search;});
+}
 
-// From: https://www.programminglogic.com/powerset-algorithm-in-c/
-template <typename t> void create_powersets(t n, std::vector<std::vector<t>>& out){
+template <typename t> bool is_sum_free(std::vector<t> const& set, std::vector<t> const& sums) {
+	return !std::any_of(sums.begin(), sums.end(), [set](auto const& sum){ return vector_contains(set, sum); });
+}
+
+template <typename t> bool is_complete(std::vector<t> const& set, std::vector<t> const& sums, t n) {
+	std::vector<int> complementary(n);
+
+	t j = 0;
+	for(t i = 0; i < n; i++) {
+		if (!vector_contains(set, i)) {
+			complementary[j] = i;
+			j++;
+		}
+	}
+
+	complementary.resize(j);
+	std::vector<bool> is_found(j);
+
+	std::cout << complementary << std::endl;
+
+	for(auto const& sum : sums) {
+		auto index = std::find(complementary.begin(), complementary.end(), sum);
+		if (index != complementary.end()) {
+			is_found[std::distance(complementary.begin(), index)] = true;
+		}
+	}
+
+	std::cout << is_found << "\n" << std::endl;
+
+	return !std::any_of(is_found.begin(), is_found.end(), [](auto const& found){return !found;}); // Check if all booleans are true
+}
+
+
+template <typename t> void get_complete_sum_free_set(t n, std::vector<std::vector<t>>& out){
 	std::vector<t> stack(n+1);
+	std::vector<t> set(n);
+	std::vector<t> sums;
+	std::vector<t> tmp;
 	long size = 0;
 	long index = 0;
 
+	for(int i = 1; i < n; i++) {
+		set[i-1] = i;
+	}
+
 	while(true){
-		if (stack[size]<n){
+		if (stack[size] < n-1){
 			stack[size+1] = stack[size] + 1;
 			size++;
 		}
@@ -65,30 +107,25 @@ template <typename t> void create_powersets(t n, std::vector<std::vector<t>>& ou
 		if (size == 0)
 			break;
 
-		out.emplace_back(size);
-		std::copy(stack.begin()+1, stack.begin()+1+size, out[index].begin());
-
-		index++;
-	}
-}
-
-template <typename t> bool vector_contains(std::vector<t> const& vec, t const& search) {
-	return std::any_of(vec.begin(), vec.end(), [search](auto const& element){return element == search;});
-}
-
-template <typename t> bool is_sum_free(std::vector<t> const& set, std::vector<t> const& sums) {
-	return !std::any_of(sums.begin(), sums.end(), [set](auto const& sum){ return vector_contains(set, sum); });
-}
-
-template <typename t> bool is_complete(std::vector<t> const& set, std::vector<t> const& sums) {
-	std::vector<bool> is_found(set.size());
-	for(auto const& sum : sums) {
-		auto index = std::find(set.begin(), set.end(), sum);
-		if (index != set.end()) {
-			is_found[std::distance(set.begin(), index)] = true;
+		for(auto i = stack.begin()+1; i < stack.begin()+1+size; i++) {
+			for(auto j = i; j < stack.begin()+1+size; j++) {
+				sums.push_back((*i + *j) % n); // Calculate the sum mod n
+			}
 		}
+
+		tmp.resize(size);
+		std::copy(stack.begin() + 1, stack.begin() + 1 + size, tmp.begin());
+
+		if(is_sum_free(tmp, sums)) {
+			if (is_complete(tmp, sums, n-1)) {
+				std::cout << "Complete Sum Free: " << tmp << std::endl;
+				out.emplace_back(size);
+				std::copy(stack.begin() + 1, stack.begin() + 1 + size, out[index].begin());
+				index++;
+			}
+		}
+		sums.clear();
 	}
-	return !std::any_of(is_found.begin(), is_found.end(), [](auto const& found){return !found;}); // Check if all booleans are true
 }
 
 template <typename t> std::vector<t> complementary_set(std::vector<t> const& set, t n) {
@@ -103,41 +140,15 @@ template <typename t> std::vector<t> complementary_set(std::vector<t> const& set
 	return out;
 }
 
-template <typename t> bool is_complete_sum_free_set(std::vector<t> const& set, t n) {
-	std::vector<t> sums;
-
-	for(long long i = 0; i < set.size(); i++) {
-		for(long long j = i; j < set.size(); j++) {
-			sums.push_back((set[i] + set[j]) % n); // Calculate the sum mod n
-		}
-	}
-
-	if (is_sum_free(set, sums)) {
-		std::vector<t> complementary = complementary_set(set, n);
-		return is_complete(complementary, sums);
-	}
-	return false;
-
-
-}
-
 
 int main() {
-	long long n = 25;
+	long long n = 15;
 
-	std::vector<std::vector<long long>> power_sets;
+	std::vector<std::vector<long long>> sum_free_sets;
 
-	create_powersets(n, power_sets);
+	get_complete_sum_free_set(n, sum_free_sets);
 
-	std::vector<std::vector<long long>> complete_sum_free_sets;
-
-	for (auto const& power_set : power_sets) {
-		if (is_complete_sum_free_set(power_set, n)) {
-			complete_sum_free_sets.push_back(power_set);
-		}
-	}
-
-	std::cout << complete_sum_free_sets << std::endl;
+	std::cout << sum_free_sets << std::endl;
 
 	return 0;
 }
