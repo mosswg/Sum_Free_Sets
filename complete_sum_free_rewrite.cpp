e51e880f24e1b1f6bd721f87c14e5b5d3c7e4265
Moss <moss@mossx.net>
2023-01-12 15:33:29 -0500

    Logic Method: Added New Non-complete Logic Method
    This method works by directly generating complete sum free sets rather
    than generating all sum free sets and checking if they're complete. I've
    worked it out on paper but this implementation is not yet working.

--- complete_sum_free_rewrite.cpp
+++ complete_sum_free_rewrite.cpp
@@ -95,6 +95,425 @@ uint64_t generate_sums(uint64_t set, int n) {
     return sums;
 }
 
+void mirror_first_half_of_set(uint64_t& set, int n) {
+    for (int i = 0; i <= (n / 2); i++) {
+        set |= ((set >> i) & 0b1) << (n - i);
+    }
+}
+
+bool is_complete_sum_free(const uint64_t set, int n) {
+    uint64_t sums = 0;
+
+    for (uint64_t i = 0; i <= n; i++) {
+        // Add i to each value of the set and or with the sums if the set contains i
+        if ((set >> i) & 0b1) {
+            sums |= rotl(set, i, n);
+
+            // Check if not sum-free
+            if ((sums & (set)) & mask) {
+                return false;
+            }
+        }
+    }
+
+    // Check if the sums are equal to the complementary set
+    return ((sums & mask) == ((~set) & mask));
+}
+
+__always_inline uint64_t& get_gp_value(uint64_t* gps, uint32_t i, uint32_t j, uint32_t n) {
+    return gps[((n * i) + j)];
+}
+
+__always_inline void set_gp_value(uint64_t* gps, uint64_t value, uint32_t i, uint32_t j, uint32_t n) {
+    gps[((n * i) + j)] = value;
+}
+
+void generate_gp(uint32_t n, uint32_t i, uint32_t& current_me_index, uint64_t* me, uint64_t* output) {
+    constexpr uint64_t one = ((uint64_t)1);
+    for (uint32_t j = i + 1; j < n; j++) {
+        output[j] = 0;
+        uint32_t j_inv = ((n + i) - j);
+        if (j_inv == j) {
+            me[current_me_index++] = (one << j) | (one << i);
+        }
+        output[j] |= (one << j_inv);
+        output[j] |= (one << j);
+    }
+    uint64_t j_mask;
+    uint64_t n_j_mask;
+    uint64_t current_mask;
+    bool invalid_current_value;
+    for (uint32_t j = 1; j < i; j++) {
+        invalid_current_value = false;
+        j_mask = (one << (j));
+        n_j_mask = (one << (i - j));
+        if (j_mask == n_j_mask) {
+            me[current_me_index++] = (one << j) | (one << i);
+        }
+        current_mask = j_mask | n_j_mask;
+        for (int k = 0; k < current_me_index; k++) {
+            if (me[k] == current_mask) {
+                invalid_current_value = true;
+                break;
+            }
+        }
+        if (!invalid_current_value) {
+            output[j] = 0;
+            output[j] = current_mask;
+        }
+    }
+}
+
+void remove_invalid_gps(uint64_t* gps, const uint64_t* me, uint32_t num_me, uint32_t n) {
+    for (uint32_t i = 1; i < n; i++) {
+        for (uint32_t j = 1; j < n; j++) {
+            for (int k = 0; k < num_me; k++) {
+                if (me[k] == get_gp_value(gps, i, j, n)) {
+                    set_gp_value(gps, 0, i, j, n);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void remove_duplicate_meps(uint64_t* me, uint32_t& current_me_index) {
+    bool is_last_value = true;
+    bool removed = false;
+    for (int32_t i = (int32_t)current_me_index - 1; i > 0; i--) {
+        for (int32_t j = 0; j < i; j++) {
+            if (me[i] == me[j]) {
+                if (is_last_value) {
+                    me[i] = 0;
+                    current_me_index--;
+                    removed = true;
+                    break;
+                }
+                else {
+                    for (int32_t k = i + 1; k < current_me_index; k++) {
+                        me[k - 1] = me[k];
+                    }
+                    me[--current_me_index] = 0;
+                    is_last_value = true;
+                }
+            }
+        }
+        if (!removed) {
+            is_last_value = false;
+        }
+        removed = false;
+    }
+}
+
+bool is_eliminated(uint64_t elimination, uint32_t value) {
+    return elimination & (((uint64_t)1) << value);
+}
+
+bool eliminate_value_from_gps(uint64_t& set, uint64_t& elimination, uint64_t* gps, uint64_t* me, uint32_t& current_me_index, uint32_t value_to_remove, uint32_t n) {
+    elimination |= ((uint64_t)1) << value_to_remove;
+    uint64_t value_mask = ((uint64_t)1) << value_to_remove;
+
+    for (int i = 1; i < n; i++) {
+        if (is_eliminated(elimination, i)) {
+            // This indicates that the value has already been eliminated
+            continue;
+        }
+        uint32_t num_values_in_gps = 0;
+        uint64_t previous_val = 0;
+        for (int j = 1; j < n; j++) {
+            uint64_t& current_gp_value = get_gp_value(gps, i, j, n);
+            if (current_gp_value && current_gp_value != previous_val) {
+                num_values_in_gps++;
+                previous_val = current_gp_value;
+            }
+        }
+        if (num_values_in_gps == 1) {
+            for (int j = 1; j < n; j++) {
+                if ((get_gp_value(gps, i, j, n) & value_mask)) {
+                    set |= (((uint64_t)1) << i);
+                    if (set & elimination) {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    return true;
+}
+
+bool remove_value_from_gps(uint64_t& set, uint64_t& elimination, uint64_t* gps, uint64_t* me, uint32_t& current_me_index, uint32_t value_to_remove, uint32_t n, uint32_t max_value = 0) {
+    uint64_t value_mask = ((uint64_t)1) << value_to_remove;
+    if (!max_value) {
+        max_value = n;
+    }
+    uint64_t mep;
+    for (int i = 1; i < n; i++) {
+        if (is_eliminated(elimination, i)) {
+            continue;
+        }
+        if ((mep = get_gp_value(gps, value_to_remove, i, n))) {
+            me[current_me_index++] = mep;
+            // If mep has only one value. alt: if mep is a power of two
+            if (!((mep) & (mep - 1))) {
+                elimination |= mep;
+            }
+            if ((mep & set) == mep) {
+                return false;
+            }
+        }
+    }
+
+    for (int i = 1; i < max_value; i++) {
+        for (int j = 1; j < n; j++) {
+            uint64_t& current_gp_value = get_gp_value(gps, i, j, n);
+            if ((current_gp_value & value_mask)) {
+                // Remove the value but leave everything else
+                // Since gps are always either two numbers or one number we check that then operate accordingly
+                current_gp_value &= ~value_mask;
+
+                if (current_gp_value != 0) {
+                    int left_over_value = 0;
+                    for (; current_gp_value >> (left_over_value + 1); left_over_value++);
+                    uint64_t new_me = (((uint64_t) 1) << i) | (((uint64_t) 1) << left_over_value);
+                    bool add_me = true;
+                    for (int k = 0; k < current_me_index; k++) {
+                        if (me[k] == new_me) {
+                            add_me = false;
+                            break;
+                        }
+                    }
+                    if (add_me) {
+                        me[current_me_index++] = new_me;
+                    }
+
+                }
+                else {
+                    eliminate_value_from_gps(set, elimination, gps, me, current_me_index, i, n);
+                    if (set & elimination) {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    remove_duplicate_meps(me, current_me_index);
+    remove_invalid_gps(gps, me, current_me_index, n);
+    return true;
+}
+
+bool check_if_could_be_valid(int check_value, uint64_t set, uint64_t &elimination, uint64_t **gps, uint64_t *me,
+                             uint32_t &current_me_index, uint32_t n) {
+
+}
+
+bool logical_elimination(uint64_t& set, uint64_t& elimination, uint64_t *gps, uint64_t* me, uint32_t& current_me_index, uint32_t n, uint32_t max_value = 0) {
+    bool empty_gps;
+    if (!max_value) {
+        max_value = n;
+    }
+
+    for (int i = 1; i < max_value; i++) {
+        if (is_eliminated(elimination, i)) {
+            continue;
+        }
+        empty_gps = true;
+        for (int j = 1; j < n; j++) {
+            if (gps[(n * i) + j] != 0) {
+                // remove invalid gps
+                uint64_t current_gp_value = get_gp_value(gps, i, j, n);
+                if ((current_gp_value & elimination) == current_gp_value) {
+                    current_gp_value = 0;
+                    continue;
+                }
+                empty_gps = false;
+                break;
+            }
+        }
+
+        if (empty_gps) {
+            set |= (((uint64_t)1) << i);
+            bool valid_set = remove_value_from_gps(set, elimination, gps, me, current_me_index, i, n, max_value);
+            if (!valid_set) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+bool solve_set(uint64_t& set, uint64_t& elimination, uint64_t* gps, uint64_t* me, uint32_t& current_me_index, uint32_t n, uint32_t previous_recursion_start = 0) {
+    if (!mask) {
+        initialize_for_n(n);
+    }
+    uint64_t previous_set = set;
+    // Valid set condition
+    while (((set | elimination) | 0b1) != mask) {
+//        std::cout << "Logical Elimination from: \n\t";
+//        print_set(set, n);
+        bool valid_set = logical_elimination(set, elimination, gps, me, current_me_index, n, previous_recursion_start);
+//        std::cout << "\t";
+//        print_set(set, n);
+//        std::cout << "\tE: ";
+//        print_set(elimination, n);
+        // Invalid set condition
+        if (!valid_set) {
+            return false;
+        }
+        else if(is_complete_sum_free(set, n)) {
+            std::cout << "found set " << current_complete_sum_free_set + 1 << " with elimination: ";
+            print_set(set, n);
+            complete_sum_free_sets[current_complete_sum_free_set++] = set;
+            return true;
+        }
+        // Branching condition
+        else if (set == previous_set) {
+            uint64_t gps_copy[n * n];
+            uint64_t me_copy[n * n];
+            uint64_t set_copy;
+            uint64_t elimination_copy;
+            uint32_t current_me_copy;
+            bool added_set = false;
+            if (previous_recursion_start == 0) {
+                previous_recursion_start = n;
+            }
+            for (uint32_t i = previous_recursion_start - 1; (previous_recursion_start != n && i > 0) || (previous_recursion_start == n && i > (n / 2)); i--) {
+                uint64_t i_mask = (((uint64_t)1) << i);
+                // don't try to add eliminated values
+                if (i_mask & elimination) {
+                    continue;
+                }
+                // avoid infinite recursion
+                else if (i_mask & set) {
+                    continue;
+                }
+                set_copy = set;
+                elimination_copy = elimination;
+                current_me_copy = current_me_index;
+                for (int j = 0; j < n; j++) {
+                    for (int k = 0; k < n; k++) {
+                        set_gp_value(gps_copy, get_gp_value(gps, j, k, n), j, k, n);
+                    }
+                    me_copy[j] = me[j];
+                }
+                set_copy |= i_mask;
+                // Avoid infinite recursion
+                if (set_copy == set) continue;
+                bool new_is_valid_set = remove_value_from_gps(set_copy, elimination_copy, gps_copy, (uint64_t*)me_copy, current_me_copy, i, n, i);
+                if (new_is_valid_set) {
+//                    std::cout << "If " << i << " in ";
+//                    print_set(set, n);
+//                    std::cout << "With elimination: ";
+//                    print_set(elimination, n);
+                    new_is_valid_set = solve_set(set_copy, elimination_copy, gps_copy, (uint64_t*)me_copy, current_me_copy, n, i);
+                }
+                if (new_is_valid_set && is_complete_sum_free(set_copy, n)) {
+                    bool add_set = true;
+                    for (int j = 0; j < current_complete_sum_free_set; j++) {
+                        if (complete_sum_free_sets[j] == set_copy) {
+                            add_set = false;
+                            break;
+                        }
+                    }
+                    if (add_set) {
+                        added_set = true;
+                        std::cout << "found set " << current_complete_sum_free_set + 1 << " with branch: ";
+                        print_set(set_copy, n);
+                        complete_sum_free_sets[current_complete_sum_free_set++] = set_copy;
+                    }
+                }
+            }
+//            std::cout << "decursing with: ";
+//            print_set(set, n);
+            return added_set;
+        }
+        previous_set = set;
+    }
+
+    return true;
+}
+
+void print_gps(uint64_t** gps, uint32_t n) {
+    for (uint32_t i = 0; i < n; i++) {
+        std::cout << "gp_" << i << " = {\n";
+        for (int j = 0; j < n; j++) {
+            print_set(gps[i][j], n);
+        }
+        std::cout << "}\n";
+    }
+}
+
+void print_meps(uint64_t* me, uint32_t num_me, uint32_t n) {
+
+    std::cout << "\nme = {\n";
+    for (int j = 0; j < num_me; j++) {
+        print_set(me[j], n);
+    }
+    std::cout << "}\n";
+}
+
+uint32_t generate_gps(uint32_t n, uint64_t* me, uint64_t* output) {
+    for (int i = 0; i < n; i++) {
+        me[i] = 0;
+    }
+    uint32_t current_me_index = 0;
+    for (uint32_t i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            set_gp_value(output, 0, i, j, n);
+        }
+        generate_gp(n, i, current_me_index, me, output + (i * n));
+
+    }
+
+    remove_invalid_gps(output, me, current_me_index, n);
+
+    return current_me_index;
+}
+
+bool check_for_asymmetric_sets(uint32_t n) {
+    uint64_t mask = (((uint64_t)1) << ((n + 1) / 2)) - 1;
+    bool found = false;
+    for (uint32_t i = 0; i < current_complete_sum_free_set; i++) {
+		uint64_t current_set = complete_sum_free_sets[i];
+		if (!current_set) {
+			continue;
+		}
+        uint64_t half_set = current_set & mask;
+        mirror_first_half_of_set(half_set, n);
+        if (half_set != current_set) {
+            std::cout << "Found asymmetric set" << std::endl;
+            print_set(current_set, n);
+            found = true;
+        }
+    }
+    return found;
+}
+
+uint32_t print_all_complete_sum_free_sets_logic(uint32_t n) {
+    uint64_t gps[n*n];
+
+    uint64_t me[n*n];
+
+    uint64_t set = 0, elimination = 0;
+
+    uint32_t num_me = generate_gps(n, me, gps);
+
+    solve_set(set, elimination, gps, me, num_me, n);
+
+    std::cout << "\nFound: \n";
+    print_sets(n);
+
+    std::cout << "Found asymmetric?: \n";
+
+    if(!check_for_asymmetric_sets(n)) {
+        std::cout << "No";
+    }
+
+    std::cout << "Found: " << current_complete_sum_free_set << std::endl;
+
+    return current_complete_sum_free_set;
+}
+
 bool is_complete(const uint64_t set, int n) {
     uint64_t sums = 0;
 
@@ -171,31 +590,6 @@ void generate_complete_sum_free_sets(int n) {
         generate_nodes_from_starting_value(i, n);
     }
     std::cout << std::endl;
-}
-
-
-bool is_complete_sum_free(const uint64_t set, int n) {
-    uint64_t sums = 0;
-
-    for (uint64_t i = 0; i <= n; i++) {
-        // Add i to each value of the set and or with the sums if the set contains i
-        if ((set >> i) & 0b1) {
-            sums |= rotl(set, i, n);
-
-            // Check if not sum-free
-            if ((sums & (set)) & mask) {
-                return false;
-            }
-        }
-    }
-
-    print_set(set, n);
-    print_set(sums & mask, n);
-
-
-
-    // Check if the sums are equal to the complementary set
-    return ((sums & mask) == ((~set) & mask));
 }
 
 int print_all_complete_sum_free_sets(int n) {
@@ -285,14 +679,9 @@ uint64_t convert(const int32_t set[]) {
 
 
 void test_against_known_values(int n) {
-    uint32_t num_found = print_all_complete_sum_free_sets_new(n);
+    uint32_t num_found = print_all_complete_sum_free_sets_logic(n);
 
     uint32_t size = number_of_sets[n];
-
-    if (num_found != size) {
-        std::cerr << "ERROR: Size mismatch in results:\n\tFound: " << num_found << "\n\tShould Find: " << size << std::endl;
-        exit(1);
-    }
 
     bool* is_found = new bool[size];
     bool missing_any = false;
